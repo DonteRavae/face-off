0fd82d3e6c85d925ecd0ca0b54cda94de65cba26
Donte Littlejohn <donteravae@mpulse.network>
2019-05-27 00:27:41 -0400

    Converted <Register /> and <SignIn /> components to class components. Finished button submit method on <App />, added a loadUser method, and created a user state.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,25 @@ class App extends Component {
       imageURL: '',
       box: {},
       route: 'signIn',
-      isSignedIn: false
+      isSignedIn: false, 
+      user: {
+        email: '',
+        id: '',
+        name: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({ user: {
+      email: data.email,
+      id: data.id,
+      name: data.name,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -65,23 +82,38 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if(response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            Object.assign(this.state.user, {entries: count});
+          })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
     if (route === 'signOut') {
-      this.setState({isSignedIn: false})
+      this.setState({isSignedIn: false});
     } else if (route === 'home') {
       this.setState({isSignedIn: true});
     }
 
-    this.setState({route: route})
+    this.setState({route: route});
   }
 
 
   render() {
-    const {isSignedIn, imageUrl, route, box} = this.state
+    const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
         <Particles className='particles'
@@ -91,8 +123,8 @@ class App extends Component {
         { route === 'home'
         ? <div>       
             <Logo />
-            <Rank />
-            <ImageLinkForm 
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm  
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onButtonSubmit} />
             <FaceRecognition
@@ -101,8 +133,8 @@ class App extends Component {
           </div>
         : (
           route === 'signIn'
-          ? <SignIn onRouteChange={this.onRouteChange} />
-          : <Register onRouteChange={this.onRouteChange} />
+          ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )}
       </div>
     );
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,17 +1,16 @@
-import React , {Component} from 'react';
+import React from 'react';
 
-export default class Rank extends Component {
-  render() {
-    return (
-      <div className=''>
-        <div className='white f3'>
-         {`Donte, your current rank is...`}
-        </div>
-
-        <div className='white f1'>
-         {`#5`}
-        </div>      
+const Rank = ({name, entries}) => {
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
       </div>
-    );
-  }
+      <div className='white f1'>
+        {entries}
+      </div>      
+    </div>
+  );
 }
+
+export default Rank;
